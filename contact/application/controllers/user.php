cc76f43c733da4fdb6acaafd346689e8ca767ef8
JUAN <JUAN@INTEL>
2016-12-13 17:48:42 -0500

    add

--- contact/application/controllers/user.php
+++ contact/application/controllers/user.php
@@ -12,12 +12,41 @@ class User extends CI_Controller {
 		$rs = $this->usuario_model->listar();
 		//$data = array("result"=>$rs);
 		//$this->load->view("usuario/listado_user",$data);
-		
 		$this->output->set_content_type('application/json')->set_output(json_encode($rs));
 	}
 	
 	public  function detail(){
+		$idUser = $this->uri->segment('3');
+		if(!is_numeric($idUser)){
+			exit();
+		}
+		echo "idUser:: ".$idUser;
+	
+		$usuario =  $this->usuario_model->obtenerById($idUser);
+		log_message('info', $this->db->last_query());
+		$rs =       $this->usuario_model->obtenerGaleriaFotos($idUser);
+		//$rs->row();
+		$rs2 =  $this->usuario_model->obtenerPerfil($idUser);
+		//print_r($usuario);
+		//exit();
 		
-		
+		log_message('info', $this->db->last_query());
+		//echo json_encode($rs2->result_array());
+		$data_user = array(
+				"nick"=>$usuario->nick,
+				"nombre"=>$usuario->nombre,
+				"presentacion"=>"2",
+				"precioBase"=>"",
+				"telefonoContacto"=>$usuario->telefono,
+				"miembroDesde"=>"",
+				
+				"fotoPrincipal"=>$usuario->foto_principal,
+				"listaFotosPublico"=>"",
+				"acercaDe"=>"",
+				"ubicacion"=>"",
+				"listaAtributos"=>"",
+				
+		);
+		$this->load->view("usuario/detalle_user",$data_user);
 	}
 }
