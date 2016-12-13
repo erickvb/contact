2e4741253ca786e396a86eaf7a18b54eed90fae2
JUAN <JUAN@INTEL>
2016-12-13 17:43:49 -0500

    add

--- contact/application/config/config.php
+++ contact/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/git/contact/contact/';
+$config['base_url'] = 'http://localhost:8083/proyecto_php/contact/';
 
 /*
 |--------------------------------------------------------------------------
