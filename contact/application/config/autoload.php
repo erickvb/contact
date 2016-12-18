0f0f4e7abccabd74515915f1418afe38e587eec2
Erick <Erick@erickServer>
2016-12-18 14:13:39 -0500

    revert

--- contact/application/config/autoload.php
+++ contact/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('usuario','galeria','database','session');
+$autoload['libraries'] = array('galeria','usuario','database','session');
 
 /*
 | -------------------------------------------------------------------
